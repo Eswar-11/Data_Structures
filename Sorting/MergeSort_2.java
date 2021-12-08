d9d10a4f0854867dd8b8c443d651aa3307532286
Eswar <56418600+Eswar-11@users.noreply.github.com>
2021-12-08 22:04:01 +0530

    Update MergeSort_2.java

--- Sorting/MergeSort_2.java
+++ Sorting/MergeSort_2.java
@@ -5,27 +5,28 @@ class  MergeSort   //Merge two sorted arrays
     public static void main(String[] args) 
     {
         Scanner sc=new Scanner(System.in);
-	      System.out.print("length_1: ");
-	      int n1=sc.nextInt();
+	System.out.print("length_1: ");
+	int n1=sc.nextInt();
         System.out.print("length_2: ");
-	      int n2=sc.nextInt();
-	      int[] arr1=new int[n1];
-	      System.out.print("Array-1: ");                    
-	      for(int b=0; b<n1; b++)
+	int n2=sc.nextInt();
+	int[] arr1=new int[n1];
+	System.out.print("Array-1: ");                    
+	for(int b=0; b<n1; b++)
         {
             arr1[b]=sc.nextInt();
         }
         int[] arr2=new int[n2];
-	      System.out.print("Array-2: ");                    
-	      for(int b=0; b<n2; b++)
+	System.out.print("Array-2: ");                    
+	for(int b=0; b<n2; b++)
         {
             arr2[b]=sc.nextInt();
-	      }
+	}
         int[] arr=new int[n1+n2];
-      
-      
+        
+        
         int i=0,j=0;
-        while(i<n1 && j<n2){
+        while(i<n1 && j<n2)
+	{
             if(arr1[i]<arr2[j])
                 System.out.print(arr1[i++]+" ");
             else
