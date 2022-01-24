d4b9bb1768a49c06976cfd0a17fdbfa25c899efa
Eswar <56418600+Eswar-11@users.noreply.github.com>
2022-01-24 22:27:18 +0530

    Create Insertion.java

---
+++ Sorting/Insertion.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class Insertion
+{ 
+    public static void main(String[] args) 
+    {
+        Scanner sc=new Scanner(System.in);
+	System.out.print("length: ");
+	int n=sc.nextInt();
+	int[] arr=new int[n];
+	System.out.print("Array: ");                    
+	for(int b=0; b<n; b++)
+        {
+            arr[b]=sc.nextInt();
+        }   
+         iSort(arr,n);
+         
+        for(int x: arr)
+	        System.out.print(x+" ");
+    }
+    static void iSort(int arr[],int n){
+    
+        for(int i=1;i<n;i++){
+            int key = arr[i];
+            int j=i-1;
+            while(j>=0 && arr[j]>key){
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1]=key;
+        }
+    }
+}
